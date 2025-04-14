c960c5f9a99489e7d3686e0e6c1bf8a86d035af1
hamza jahangir <hamzahashmi640@gmail.com>
2025-04-14 19:31:45 +0500

    c

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Disable ESLint during builds
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  // Disable TypeScript type checking during builds
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
